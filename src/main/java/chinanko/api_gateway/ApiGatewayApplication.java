2943dd90df1851f0e783e8c67fe88475dfd4914c
Antony Daniel Muñoz Leal <antonyleal280203@gmail.com>
2025-12-04 15:39:11 -0600

    first commit

--- src/main/java/chinanko/api_gateway/ApiGatewayApplication.java
+++ src/main/java/chinanko/api_gateway/ApiGatewayApplication.java
@@ -2,10 +2,9 @@ package chinanko.api_gateway; // Ojo con tu paquete
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient; // Importar
 
 @SpringBootApplication
-@EnableDiscoveryClient // <--- Pon esto para que Eureka lo vea
+//@EnableDiscoveryClient // <--- Pon esto para que Eureka lo vea
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
